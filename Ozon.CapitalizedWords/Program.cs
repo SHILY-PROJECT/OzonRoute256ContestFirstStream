464b6a6238c0cb3e3281b07814b0b05957598168
SHILY <45015327+SHILY-PROJECT@users.noreply.github.com>
2021-10-06 22:02:37 +0300

    -добавил задание "Слова с прописной буквы"; -добавил описание

---
+++ Ozon.CapitalizedWords/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Ozon.CapitalizedWords
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string line;
+
+            if (!string.IsNullOrWhiteSpace(line = Console.ReadLine()))
+                Console.WriteLine(string.Join(" ", line.Split(' ')
+                    .Select(x => new string(x.ToLower().Select((value, index) => index == 0 ? char.ToUpper(value) : value).ToArray()))));
+        }
+    }
+}
+
+/*------------------------------СЛОВА С ПРОПИСНОЙ БУКВЫ------------------------------
+-----Ограничения:
+        по времени: 2 сек
+        по памяти:  128 МБ
+
+-----Условие:
+        На стандартный вход подается строка, содержащая слова, разделенные пробелами.
+        Необходимо сделать заглавной буквой начало каждого слова в строке.
+        Вывести на стандартный вывод.
+
+-----Формат входных данных:
+        Строка
+
+-----Формат выходных данных:
+        Строка
+
+-----Примеры:
+        Входные данные:  hEllo world! 
+        Выходные данные: Hello World!
+ *///---------------------------------------------------------------------------------
